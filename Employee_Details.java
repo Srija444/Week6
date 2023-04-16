0b4243100386e637e34bb39a90821e0a5b7303b9
Srija444 <127313392+Srija444@users.noreply.github.com>
2023-04-16 03:12:00 -0700

    Add files via upload

---
+++ Employee_Details.java
@@ -0,0 +1,64 @@
+package studyopedia;
+
+
+public class Employee_Details {
+	private String name,job,workPlace;
+      private long idNumber;
+      private int salary;
+	
+      
+      
+      public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	
+	public String getJob() {
+		return job;
+	}
+	public void setJob(String job) {
+		this.job = job;
+	}
+	
+	
+	
+	
+	public String getWorkPlace() {
+		return workPlace;
+	}
+	public void setWorkPlace(String workPlace) {
+		this.workPlace = workPlace;
+	}
+	
+	
+	
+	
+	public long getIdNumber() {
+		return idNumber;
+	}
+	public void setIdNumber(long idNumber) {
+		this.idNumber = idNumber;
+	}
+	
+	
+	
+	
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+
+		
+		
+		
+	
+
+}
