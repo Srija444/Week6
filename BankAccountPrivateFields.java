135e5131b14ca4518185d6481448e2d323f170e9
Srija444 <127313392+Srija444@users.noreply.github.com>
2023-04-16 03:09:53 -0700

    Add files via upload

---
+++ BankAccountPrivateFields.java
@@ -0,0 +1,90 @@
+package studyopedia;
+
+public class BankAccountPrivateFields {
+	
+	String branch_name,account_holder_name,address;
+	long account_number,savings,IFSC_number,pho_no;
+	
+	
+	
+	
+	
+	public String getBranch_name() {
+		return branch_name;
+	}
+	public void setBranch_name(String branch_name) {
+		this.branch_name = branch_name;
+	}
+	
+	
+	
+	
+	
+	public String getAccount_holder_name() {
+		return account_holder_name;
+	}
+	public void setAccount_holder_name(String account_holder_name) {
+		this.account_holder_name = account_holder_name;
+	}
+	
+	
+	
+	
+	
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+	
+	
+	
+	public long getAccount_number() {
+		return account_number;
+	}
+	public void setAccount_number(long account_number) {
+		this.account_number = account_number;
+	}
+	
+	
+	
+	
+	
+	
+	public long getSavings() {
+		return savings;
+	}
+	public void setSavings(long savings) {
+		this.savings = savings;
+	}
+	
+	
+	
+	
+	
+	public long getIFSC_number() {
+		return IFSC_number;
+	}
+	public void setIFSC_number(long iFSC_number) {
+		IFSC_number = iFSC_number;
+	}
+	
+	
+	
+	
+	public long getPho_no() {
+		return pho_no;
+	}
+	public void setPho_no(long pho_no) {
+		this.pho_no = pho_no;
+	}
+	
+	
+	
+	
+	
+
+}
