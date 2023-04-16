77939de7597470617cc2c8339d603546b92f6048
Srija444 <127313392+Srija444@users.noreply.github.com>
2023-04-16 03:18:10 -0700

    Add files via upload

---
+++ TextBookData.java
@@ -0,0 +1,67 @@
+package studyopedia;
+
+public class TextBookData {
+	
+	
+	String book_title,author,publisher;
+	int edition;
+	long isbn_number;
+	
+	
+	
+	
+	public String getBook_title() {
+		return book_title;
+	}
+	public void setBook_title(String book_title) {
+		this.book_title = book_title;
+	}
+	
+	
+	
+	
+	
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	
+	
+	
+	
+	public String getPublisher() {
+		return publisher;
+	}
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	
+	
+	
+	
+	
+	public int getEdition() {
+		return edition;
+	}
+	public void setEdition(int edition) {
+		this.edition = edition;
+	}
+	
+	
+	
+	
+	public long getIsbn_number() {
+		return isbn_number;
+	}
+	public void setIsbn_number(long isbn_number) {
+		this.isbn_number = isbn_number;
+	}
+	
+	
+	
+	
+
+}
