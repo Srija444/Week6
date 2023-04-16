316ed98d876b2875f9e784c417d192061a249209
Srija444 <127313392+Srija444@users.noreply.github.com>
2023-04-16 03:17:54 -0700

    Add files via upload

---
+++ Swiggy.java
@@ -0,0 +1,61 @@
+package studyopedia;
+
+public class Swiggy {
+
+	String customerName,address,orderItem;
+	long phoneNumber;
+    int houseNo;
+	
+    
+    
+    
+    public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	
+	
+	
+	
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+	
+	
+	public String getOrderItem() {
+		return orderItem;
+	}
+	public void setOrderItem(String orderItem) {
+		this.orderItem = orderItem;
+	}
+	
+	
+	
+	
+	public long getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	
+	
+	public int getHouseNo() {
+		return houseNo;
+	}
+	public void setHouseNo(int houseNo) {
+		this.houseNo = houseNo;
+	}
+    
+	
+	
+	
+}
