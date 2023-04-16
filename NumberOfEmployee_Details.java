ac20f06b477b4c52d833f6b01ca57482206eb682
Srija444 <127313392+Srija444@users.noreply.github.com>
2023-04-16 03:14:14 -0700

    Add files via upload

---
+++ NumberOfEmployee_Details.java
@@ -0,0 +1,52 @@
+package studyopedia;
+import java.util.*;
+public class NumberOfEmployee_Details {
+String name,job,place;
+long id;
+int salary;
+   
+  Scanner s=new Scanner(System.in);
+  int n=s.nextInt();
+	
+	Employee_Details[] arr=new Employee_Details[n];
+	public void empdetails() {
+		Scanner s=new Scanner(System.in);
+		
+		
+		
+		for(int i=0;i<arr.length;i++) {
+			Employee_Details details=new Employee_Details();
+			System.out.println("enter employee name: ");
+			details.setName(s.next());
+			System.out.println("enter employee job: ");
+			details.setJob(s.next());
+			System.out.println("enter employee workplace: ");
+			details.setWorkPlace(s.next());
+			System.out.println("enter employee id number: ");
+			details.setIdNumber(s.nextLong());
+			System.out.println("enter employee salary: ");
+			details.setSalary(s.nextInt());
+			arr[i]=details;
+		}
+	}
+	
+	
+	public void employee_get_details() {
+		for(int i=0;i<arr.length;i++) {
+			System.out.println("--------------");
+			System.out.println("employee "+(i+1)+" details: ");
+			System.out.println("employee name: "+arr[i].getName());
+		System.out.println("employee job role: "+arr[i].getJob());
+		System.out.println("employee work_place: "+arr[i].getWorkPlace());
+		System.out.println("employee id number: "+arr[i].getIdNumber());
+		System.out.println("employee salary: "+arr[i].getSalary());
+		}
+	}
+	
+	public static void main(String args[]) {
+		NumberOfEmployee_Details details=new NumberOfEmployee_Details();
+		details.empdetails();
+		details.employee_get_details();
+	}
+	
+}
